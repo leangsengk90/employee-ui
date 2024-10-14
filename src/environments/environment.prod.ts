a9351f5ba40deae449bfbd89bc6da3d2c3b6e9d6
leangsengk90 <48117906+leangsengk90@users.noreply.github.com>
2024-10-15 02:24:05 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: "https://api.work-shop/api/",
-  PHOTO_URL: "https://api.work-shop/Photos/"
+  API_URL: "https://api.work-shop.com/api/",
+  PHOTO_URL: "https://api.work-shop.com/Photos/"
 };
